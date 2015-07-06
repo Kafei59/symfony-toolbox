ea064e7d9982d450d0978da73be2785dc43e73b2
Kafei59 <pierrick.gicquelais@epitech.eu>
2015-07-06 22:13:41 +0200

    Modify create_project.sh to launch a first composer update and a cache clear in the project directory

--- create_project.sh
+++ create_project.sh
@@ -2,22 +2,34 @@
 # @Author: gicque_p
 # @Date:   2015-07-06 21:13:24
 # @Last Modified by:   gicque_p
-# @Last Modified time: 2015-07-06 21:53:26
+# @Last Modified time: 2015-07-06 22:10:35
 
 if [ -z "$1" ]
   then
-    echo "[USAGE] ./create_project.sh: <project_folder>"
+    echo "[USAGE] $0: <project_folder>"
     exit 1
+fi
+
+if [ -d "$1" ]
+	then
+	echo "Folder already exists"
+	exit 1
 fi
 
 symfony new $1
 php $1/app/check.php
 php $1/app/console generate:bundle --namespace=$1/Bundle/CoreBundle --bundle-name=$1CoreBundle --dir=$1/src/ --format=yml --structure --no-interaction
 
-rm -rf $1/app/cache/*
-rm -rf $1/app/logs/*
+echo "\n$1_core_index:\n    path:     /\n    defaults: { _controller: $1CoreBundle:Default:index, name: World }" >> $1/src/$1/Bundle/CoreBundle/Resources/config/routing.yml
+
+cd $1
+sudo composer update
+cd ..
+
+sudo rm -rf $1/app/cache/*
+sudo rm -rf $1/app/logs/*
+
+php $1/app/console cache:clear --env=prod
 
 chmod -R 777 $1/app/cache/
 chmod -R 777 $1/app/logs/
-
-echo "\n$1_core_index:\n    path:     /\n    defaults: { _controller: $1CoreBundle:Default:index, name: World }" >> $1/src/$1/Bundle/CoreBundle/Resources/config/routing.yml
