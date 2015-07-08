cc37619ea901af42164c8e83daaf77020b6c611d
Kafei59 <pierrick.gicquelais@epitech.eu>
2015-07-08 11:44:03 +0200

    Add self-update for composer and symfony in install_symfony.sh

--- install_symfony.sh
+++ install_symfony.sh
@@ -2,10 +2,13 @@
 # @Author: gicque_p
 # @Date:   2015-07-06 21:18:26
 # @Last Modified by:   gicque_p
-# @Last Modified time: 2015-07-06 22:56:57
+# @Last Modified time: 2015-07-08 10:39:31
 
 sudo curl -LsS http://symfony.com/installer -o /usr/local/bin/symfony
 sudo chmod a+x /usr/local/bin/symfony
 
 php -r "readfile('http://symfony.com/installer');" > symfony
 php -r "eval('?>'.file_get_contents('http://getcomposer.org/installer'));"
+
+symfony self-update
+composer self-update
